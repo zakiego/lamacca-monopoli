c4d1c194fbb72ad24d88ac7c8a21b7ceb77165c0
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-06-20 22:57:15 +0800

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Hellow</h1>
+      <h1>Hellow</h1>
+      <h1>Hellow</h1>
+      <h1>Hellow</h1>
+      <h1>Hellow</h1>
+      <h1>Hellow</h1>
+    </div>
+  );
+}
+
+export default Home;
